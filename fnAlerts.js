88d03c1d14e0a8fc6236c06b75c9eb10f1b4f521
yukitakeda923 <46566829+yukitakeda923@users.noreply.github.com>
2019-03-26 14:06:08 -0400

    Add files via upload

---
+++ fnAlerts.js
@@ -0,0 +1,15 @@
+
+//This is a function for Positive notifications
+
+ function notifyYes(){
+   alert ("you won a conference tournament")
+ }
+
+/*
+Author: Yuki Takeda
+DateCreated
+*/
+
+ function notifyNo(){
+   alert ("you lost a conference tournament")
+ }
