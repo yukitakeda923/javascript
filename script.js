2be430bed4a1f04178d06a35891138f487e42748
yukitakeda923 <46566829+yukitakeda923@users.noreply.github.com>
2019-04-08 20:58:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function hideit() {
+  document.getElementById("soccer").className = "hidden";
+}
+
+function changeText() {
+  document.getElementById("p1").style.fontSize = "7em";
+  document.getElementById("p1").style.fontStyle = "Italic";
+  document.getElementById("p1").style.backgroundColor = "yellow";
+  document.getElementById("p1").style.fontweight = "bold";
+//this is for the 4.html file
+document.getElementById("persons").style.fontSize = "7em";
+document.getElementById("persons").style.fontStyle = "Italic";
+document.getElementById("persons").style.backgroundColor = "yellow";
+document.getElementById("persons").style.fontweight = "bold";
+}
+
+function refresh(){
+  location.reload();
+}
+
+//this is for the 5.html file
+function alterText(){
+  document.getElementById("persons").style.fontSize = "7em";
+  document.getElementById("persons").style.fontStyle = "Italic";
+  document.getElementById("persons").style.backgroundColor = "yellow";
+  document.getElementById("persons").style.fontweight = "bold";
+}
